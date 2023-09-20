ae96065a13982cbb3c257bca38411a223bdb511e
goldenshark0702 <125573528+goldenshark0702@users.noreply.github.com>
2023-09-20 14:51:14 +0900

    Add files via upload

---
+++ bids.js
@@ -0,0 +1,217 @@
+const bids = [
+  {
+    title: "Introduce Maksym Zuiev ",
+    content: [
+      "❤️Hello❤️",
+      "maksymzuievdev@gmail.com",
+      "16 Jun 1990.",
+      "###",
+      "This is Maksym Zuiev.",
+      "I am a Senior Engineer with 8+ years of experience in the fields of Python, Java/Javascript, DevOps/MLOps, Artificial Intelligence and AWS.",
+      "I Also have expertise in writing smart contracts and web3 integration. <br>Additionally, I have 2 years of experience working with Azure and GCP. <br>Most recently, I was employed at Katana Inu and IBM Global Services, where I worked with Kubeflow and Kubernetes to ensure efficient operation by employing up-to-date technology.",
+      "My blockchain and Artificial Intelligence career started with Teahouse Finance.When I worked as a engineer in that company, I wrote many smart contracts like Staking and Dex contracts using ML.",
+      "And after I finished my career in Teahouse Finance, I joined the Bird.Money.<br>I participated in building an unique oracle system.<br>I have built a new oracle system that allows our main Smart Contract to catch online data securely using built-in oracle nodes and the consensus algorithm with AI.",
+      "And I worked at KatanaInu as a Senior Blockchain Architect and Python engineer with Artificail intellegence from December last year.<br>I'd love to brief my experience at KatanaInu.<br>Since, It was my great career.<br>KatanaInu is a Play-2-Earn royale PC game company. All characters are NFT. KatanaInu have 5 technical members and 30 strong marketing members. I designed the architecture of the Katana eco-system and developed a project governance token using Articifial Intelligence. Then I've built NFT minting and marketplace. As you can see my avatar, they gave me their company avatar.",
+      "Now I am actively looking for blockchain and Artificial Intelligence jobs and projects.",
+      "My experience with software development has included designing efficient architecture for large applications, developing code using various libraries and frameworks, and implementing creative solutions for scalability, performance, and security. Further, I have also implemented predictive models with the likes of TensorFlow, Keras, and PyTorch, improving the accuracy and effectiveness of enterprise-grade models.<br>I’m passionate about working in the field of Artificial Intelligence and Machine Learning and have worked on numerous projects, incorporating Natural Language Processing and Computer Vision algorithms. Moreover, I have expertise in creating complex ML models and transfer learning for image classification tasks. <br>Overall, I have a strong track record in hands-on development work which is augmented by my understanding of DevOps principles. As I have experience working with both cloud platforms (AWS, Azure, and GCP) as well as open source technologies, I’m confident that my exposure to different approaches to system design will be beneficial to the team.<br>If you are looking for an experienced AI Engineer who can bring innovation and energy to your team, I would be happy to discuss my experience further. <br>Thank you for your time and consideration. ",
+      ""
+    ],
+  },
+  {
+    title: "What is reason you left at KatanaInu(Maksym Zuiev)",
+    content: [
+      "❤️Hello❤️",
+      "I left KatanaInu as Senior Blockchain Architect and Python Engineer with Artificial Intelligence at the end of June 2023 in order to pursue other educational and career opportunities. I felt t hat a change in environment would give me more professional growth potential.<br>The main reason for leaving KatanaInu as a Senior Blockchain Architect and Python engineer with Artificial Intelligence is to pursue a different career opportunity, such as a new position in a different field or an entirely new career. <br> And then As I said, KatanaInu is NFT PC game company. governance token, staking, NFT minting and Marketplace...<br>All technical problems are resolved successfully.<br>All that remains was to complete the marketing . So I left company.",
+      ],
+  },
+  {
+    title: "Why apply to my company(Maksym Zuiev)",
+    content: [
+      "❤️Hello❤️",
+      "first: I liked company slogan.<br> 'We consider people as talent, not resources'<br><br>second: Thanks for Anna's hand.<br>She lead me in this lucky chance.",
+    ],
+  },
+  {
+    title: "what is your the best difficult project with AI in game company(Maksym Zuiev)",
+    content: [
+      "❤️Hello❤️",
+      "My first project with AI in a game company would be to create an AI-powered game assistant. <br>This assistant would use machine learning and natural language processing (NLP) to help players with game-related queries. <br>It could answer questions about gameplay mechanics, provide tips and hints on how to progress in the game, or even suggest more engaging experiences tailored to each individual player. <br>The goal of this project would be to give players a more immersive experience with our game, while also making it easier for them to get the help and guidance they need.",
+    ],
+  },
+  {
+    title: "how to solve problem create an AI-powered game assistant(Maksym Zuiev)",
+    content: [
+      "❤️Template❤️",
+      "In order to create an AI-powered game assistant, I first identified the core features of the game and the challenges that players faced. <br>I then researched ways of using AI technology to better assist in solving these problems. <br>This included researching existing AI technologies as well as programming tools and APIs. <br>Once I had a better understanding of how AI could be used, I developed an AI-based solution that provides automated assistance with in-game tasks. <br>sssFinally, I implemented the AI-powered game assistant into the game and tested its performance.",
+    ],
+  },
+  {
+    title: "what is your first project with AI(Maksym Zuiev)",
+    content: [
+      "❤️Template❤️",
+      "My first project with AI was creating an application that leveraged machine learning to identify objects in images. <br>The application used convolutional neural networks to recognize objects and could identify the types of objects, as well as their locations in the image. <br>It was a fun and interesting project to work on, and I learned a lot about how AI can be applied to solve real-world problems.",
+    ],
+  },
+  {
+    title: "The describe to Convert English to Hindi with this Python Package it return String  useful for NLP/ML Projects using python.(Maksym Zuiev)",
+    content: [
+      "❤️Template❤️",
+      "Hi.<br>Nice to meet you.<br>I complete your text recongization project using the Python Package AITranslit,I developed last years ago to Convert English to Hindi within 5 hours yesterday.<br>As tell more about AITranslit package, I develped last years ago, emmm...<br>The AITranslit package is a powerful and easy to use Python package for converting English text into corresponding Hindi texts. <br>This package can be used for Natural Language Processing (NLP) and Machine Learning (ML) projects that require the conversion of English words or sentences into Hindi. <br>I used know-how skills and high-technology to develop TTP(Text translate package).<br>The AITranslit package consists of a set of functions and classes to support various linguistic operations like transliteration, morphological analysis and generation. <br>The library implements an algorithm for mapping English words or phrases to their Hindi equivalents. <br>In addition, the package supports various other features like automatic spell-check and correction, automatic hyphenation and sentence segmentation.  <br>The basic usage of the my AITranslit package is very simple and useful. <br>All it need to do is call the ‘transliterate’ function with your English input string as argument and the output will be the corresponding Hindi transliteration.<br>For example, the following code snippet translates the English phrase “How are you?” to Hindi: <br><br>//// hindi = transliterate(“How are you?”) <br><br>The output of the above code snippet will be “흐2587 었어?”(aap kaise hain?) which is the equivalent of the English phrase in Hindi. <br>Apart from this basic usage, the AITranslit package also provides various advanced options to customize the translation process. <br>For example, you can specify the language target for the translation by specifying the ‘target_language’ parameter while calling the ‘transliterate’ function. <br>Similarly, you can also provide a list of words or phrases to take special care while translating. <br>Overall, the AITranslit package is a useful tool for Natural Language Processing and Machine Learning projects that require English to Hindi translation. It provides a simple and intuitive interface for completing the transliteration process with minimal effort.<br>That's all, Sathwika",
+    ],
+  },
+  {
+    title: "Template(Who)",
+    content: [
+      "❤️Template❤️",
+      "",
+    ],
+  },
+  {
+    title: "Template(Who)",
+    content: [
+      "❤️Template❤️",
+      "",
+    ],
+  },
+  {
+    title: "Bid For AutoBid",
+    content: [
+      "❤️Hello❤️",
+      "I have already done the same work that you want many times.",
+      "As a professional Software Engineer, I have rich experience and depth knowledge of this field.",
+      "While reading job description, I'm very exciting that this job seems to be posted for me.",
+      "All required skills mentioned on job description are my strongest ones as well as I am very confident with them",
+      "I could start to work immediately depending on your requirements.",
+      "Looking forward to work on this project.",
+    ],
+  },
+  {
+    title: "Figma Design",
+    content: [
+      "❤️Hello❤️",
+      "I have seen your project and I will be pleased on working your proposal.",
+      "I graduated from Savannah College of Art and Design with a Bachelor of Fine Arts degree with a major in Graphic design and a minor in advertising and branding. I have several years of design and development experience. I am also efficient at communicating and visualizing my work towards others methodically and within deadlines. Moreover, I have a keen eye for details and aesthetics. You can check my recent Figma design work.",
+      "https://www.figma.com/file/zXvEYCzhOrBNSLUHi9DZon/ecommerce-website-design",
+      "https://www.figma.com/file/8eUHdZAH8jn1nefFTe51mR/GreenGuard",
+      "https://www.figma.com/file/I6HhgL7RVSlRqvXKt1mMTH/Fashion-Style",
+      "https://www.figma.com/proto/I6HhgL7RVSlRqvXKt1mMTH/Fashion-Style?node-id=1%3A759&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A759",
+      "You will not be regretted on hiring me for this job proposal as I am a creative designer with pitch ideas. I have completed all my previous works by handling them smoothly and professionally. I respect all reviews and criticisms about my work and I spend the most effort on providing the best result.",
+
+      "So contact me if you think I am compatible enough for this job.",
+
+      "Thank you.",
+    ],
+  },
+  {
+    title: "Bid For Front End",
+    content: [
+      "❤️Hello❤️",
+      "I have already worked as frontend developer for other companies and finish many frontend project perfectly.",
+      "You can check it  on my Github: https://github.com/BlueSpider0130",
+      "As a professional web developer, I have rich experience and depth knowledge of React, Vue Angular with Figma, XD, PSD",
+      "I could start to work immediately depending on your requirements.",
+      "I believe in quality over quantity, long-term relationships over a short-term relationship and I respect all reviews and criticisms about my work and I spend the most effort on providing the best result.",
+      "Looking forward to work on this project.",
+    ],
+  },
+  {
+    title: "Bid For Figma to HTML",
+    content: [
+      "❤️Hello❤️",
+      "As a professional web developer, I have rich experience and depth knowledge of React, Vue Angular with Figma, XD, PSD",
+      "I could start to work immediately depending on your requirements. And I'll provide a full-responsive Website on all devices (Desktops, Laptops, Tablets, Mobiles)",
+      "I believe in quality over quantity, and I respect all reviews and criticisms about my work and I spend the most effort on providing the best result.",
+      "Looking forward to work on this project.",
+    ],
+  },
+  {
+    title: "Bid For Laravel",
+    content: [
+      "❤️Hello❤️",
+      "As a professional web developer, I have rich experience and in-depth knowledge of Laravel. You can check my past Laravel works: ",
+      "https://github.com/ProDev-Web/Rewards-Dacor (live website link https://rewardsdacor.com/)",
+      "https://github.com/ProDev-Web/LoanSystem",
+      "I could start to work immediately depending on your requirements.",
+      "I believe in quality over quantity, and I respect all reviews and criticisms about my work and I spend the most effort on providing the best result.",
+      "Looking forward to work on this project.",
+    ],
+  },
+  {
+    title: "Bid for WordPress",
+    content: [
+      "❤️Greeting❤️",
+      "I hope you are doing good! I am  a Senior WordPress Developer.",
+      "I have the exact match with the skills you are looking for a current project.",
+
+      "I have completed 100+ projects.... without letting down a single Client......",
+      "As Many Clients are still in touch based on my Working strategies and their satisfaction on their all previous Projects.",
+
+      "Kindly have a look at some of my previous work samples:",
+      "https://whalies.club",
+      "https://riotflavours.com/",
+      "https://atctower.com/",
+      "https://stanbranding.com/",
+      "https://startempirewire.com",
+      "https://seattleshoulderdoc.com",
+
+      "I have 10+ years of Experience with : Graphic & Web-Design| Bootstrap| Responsive HTML5|CSS| jQuery|",
+      "Laravel | PHP| Wordpress: wooCommerce| Magento| OpenCart| Prestashop| Joomla| SEO Services.",
+      
+      "WHY-ME:",
+      "» No Upfront-Amount needed to Begin your Project",
+      "» Ready to Start as Immediate::,Unlimited Revisions & Proper Testing",
+      "» Keep your Data & Website Secure::, Daily Progress Updates",
+      "» You will get FREE BUG-SUPPORT even after Project-Completion",
+
+      "Let's have a quick conversation on the details to proceed further with this project.",
+      "Regards",
+    ],
+  },
+  {
+    title: "Bid For Full Stack Developer",
+    content: [
+      "❤️Hello❤️",
+      "✨ FULL STACK  DEVELOPER ✨",
+      "I can surely help you develop a fully functional website as per your requirement with 100% job satisfaction. ",
+      "I am a ⚡creative freelancer⚡ and Since 2012, I am managing Software Development tasks successfully. and I have more than 👍10 years of Full stack experience and development for different companies in  👍Europe.  As a highly skilled Full-Stack Web developer with 6+ years of working experience. ",
+
+      "⭐ Web ⭐",
+      "✔️ Frontend : AngularJS, ReactJS, VueJS, Gatsby, Typescript",
+      "✔️ Backend : PHP/Laravel-CodeIgnitor-YII2, Python/Django, Node.js/Express, Ruby/Ruby on Rails, C#/ASP.NET, GraphQL",
+      "✔️ Database : MySQL, NoSQL Couch & MongoDB, SQLite, PostgreSQL",
+      "✔️ Some more",
+      "★ C/C++, C#, Java, Python, R, Go, Rust, Ruby, Dart",
+      "★ Software Architecture, Algorithm, Web Scraping",
+      "★ PSD to HTML, AJAX, Javascript, Responsive Design",
+      "★ PHP, HTML, jQuery, Bootstrap, SCSS, CSS and Less",
+
+      "Hope to see you on my client list. Waiting for your response.",
+      "Best Regards.",
+    ],
+  },
+  {
+    title: "Bid For Game Developer",
+    content: [
+      "❤️Hello❤️",
+      "how's it going, I hope you are doing well and staying safe.",
+      "I am Professional Game Developer.",
+      "I have read your project description seriously.",
+      "I really want to participate in this work, and if I do, I feel like I can achieve amazing results.",
+      "With 8 years of Unity experience for global businesses, I offer the technical expertise you might be looking for.",
+      "Known for creating robust, high-speed web and mobile Games, my game development work has helped grow revenues, accelerate customer acquisition, increase game traffic and deliver an industry-leading competitive advantage. I highly value quality over quantity and I put a big effort to create a truly qualified product.",
+      "I love pixel-perfectness, responsiveness and enjoy writing tests.",
+      "This had been an important factor that made my previous projects successful.",
+
+      "Please review my resume, which provides details of these projects and examples of skills in:",
+      "- Game development using Unity, C# Programming.",
+      "- Unreal Engine, Blueprint",
+      "- Translation of wireframes and rapid prototyping tools to accurate, working code",
+      "- Server Backend Programming using PUN2",
+      "- AR/VR game by Oculus Quest.",
+
+      "If my skills and experience match your requirements, just click chat button to discuss your project details.",
+      "I am willing to take a technical interview, technical test, or trial project, to verify myself.",
+      "Best Regards.",
+    ],
+  },
+];
